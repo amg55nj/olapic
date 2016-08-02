ae1b9402c7230da178bb818c6776e07951da046e
Rob Kumar <amg55nj@gmail.com>
2016-08-02 12:02:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 	$(document).ready(function() {
 		$.getJSON('http://photorankapi-a.akamaihd.net/customers/215757/media/photorank?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2', function(olaResults) {
 			$('<div class="results"></div>')
-			.append(olaResults.metadata.code)
-			.appendTo('body')
+			.append(olaResults.data.images.normal)
+			.appendTo('.slider-container')
 		});
 	});
